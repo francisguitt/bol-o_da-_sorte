c4b927499ff52bd0aaaadf2684020d272cb523ec
Guitt Zoom <77627405+francisguitt@users.noreply.github.com>
2023-12-16 13:46:44 -0300

    change placehold input

--- src/components/NumberInput.js
+++ src/components/NumberInput.js
@@ -6,7 +6,7 @@ const NumberInput = ({ value, onChange, }) => {
     return (
         <div className="number-input">
             <input className='input_element'
-            placeholder="Digite Seu Numero"
+            placeholder="apenas de 1 a 25 números "
                 type="number"
                 value={value}
                 onChange={(e) => onChange(e.target.value)}
